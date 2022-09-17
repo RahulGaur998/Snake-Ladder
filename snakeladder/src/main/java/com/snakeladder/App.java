2748a94deaf4e0ab41651b98bff12175f0db6ee9
RahulGaur998 <gaur.rahul996@gmail.com>
2022-09-17 12:28:00 +0530

    ADD: Added functionality to play with 2 players and refactored code to accomodate logic to play with 2 players

--- snakeladder/src/main/java/com/snakeladder/App.java
+++ snakeladder/src/main/java/com/snakeladder/App.java
@@ -7,6 +7,7 @@ class Player {
     private static final Logger logger = LogManager.getLogger(App.class);
     private int playerPosition;
     int numberOfRolls = 0;
+    int option = 0;
 
     public int getPlayerPosition() { // getter to get playerPosition value
         return playerPosition;
@@ -22,13 +23,18 @@ class Player {
     }
 
     int checkOption(int dieValue) { // checks whether no-play/ladder/snake using random and switch
-        int option = (int) Math.floor(Math.random() * (2 - 0 + 1) + 0);
+        option = (int) Math.floor(Math.random() * (2 - 0 + 1) + 0);
         switch (option) {
             case 0: // if option generates 0 do nothing
                 break;
             case 1:
-                setPlayerPosition(getPlayerPosition() + dieValue); // if option generates 1 add dieValue to
+                setPlayerPosition(getPlayerPosition() + dieValue); // if option generates 1 add dieValue to //
                                                                    // playerPosition
+                if (getPlayerPosition() > 100) { // if playerPosition goes beyond 100 decrement to previous value
+                    // by subtracting with die value
+                    setPlayerPosition(getPlayerPosition() - dieValue);
+                    break;
+                }
                 break;
             case 2:
                 playerPosition = playerPosition - dieValue; // if option generates 2 minus dievalue from playerPosition
@@ -42,23 +48,14 @@ class Player {
         return playerPosition; // return player position
     }
 
-    void play(Player playerObj) { // play method expects a playerObject and works accordingly
-        while (playerObj.getPlayerPosition() != 100 && playerObj.playerPosition < 100) { // play until player position
-                                                                                         // becomes 100
+    void play(Player player) { // play method expects a playerObject and works accordingly
+        // becomes 100
+        int dieValue = player.playerRollsDie();
+        logger.info("Rolling a die player get : " + dieValue);
+        logger.info(
+                "After Checking options NoPlay/ Ladder/ Snake player position is: "
+                        + player.checkOption(dieValue));
 
-            int dieValue = playerObj.playerRollsDie();
-            logger.info("Rolling a die player get : " + dieValue);
-            logger.info(
-                    "After Checking options NoPlay/ Ladder/ Snake player position is: "
-                            + playerObj.checkOption(dieValue));
-            if (playerObj.getPlayerPosition() > 100) { // if playerPosition goes beyond 100 decrement to previous value
-                                                       // by subtracting with die value
-                playerObj.setPlayerPosition(playerObj.getPlayerPosition() - dieValue);
-            } else if (playerObj.getPlayerPosition() == 100) {// if playerPosition is 100 print player Wins
-                logger.info("Player Wins!!! Player position is :" + playerObj.getPlayerPosition());
-                logger.info("Total Number of rolls is: " + playerObj.numberOfRolls);// display total number of die rolls
-            }
-        }
     }
 
     Player() { // constructor to initialize playerPosition as zero
@@ -66,13 +63,59 @@ class Player {
     }
 }
 
+class PlayWith2Players {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    void playerWins(Player p1, Player p2) { // when either player reaches playerPosition 100
+        if (p1.getPlayerPosition() == 100) {
+            logger.info("Player 1 wins!!");
+            logger.info("Total number of rolls are :" + p1.numberOfRolls);
+            System.exit(0);
+        } else if (p2.getPlayerPosition() == 100) {
+            logger.info("Player 2 wins!!");
+            logger.info("Total number of rolls are :" + p1.numberOfRolls);
+            System.exit(0);
+        }
+    }
+
+    void playWith2Players(Player p1, Player p2) {
+        while (p1.getPlayerPosition() < 100 && p2.getPlayerPosition() < 100) { // play until either player's
+            // playerPosition reaches 100
+
+            logger.info("Player1's turn :");
+            p1.play(p1);
+            while (p1.option == 1) { // if player1 gets option 1 i.e. Ladder play again
+                if (p1.getPlayerPosition() == 100) {// check if player reaches 100
+                    playerWins(p1, p2);
+                }
+                logger.info("Player1's turn :");
+                p1.play(p1);
+            }
+            logger.info("Player2's turn :");
+            p2.play(p2);
+            while (p2.option == 1) { // if player2 gets option 1 i.e. Ladder play again
+                if (p2.getPlayerPosition() == 100) {
+                    playerWins(p1, p2);
+                }
+                logger.info("Player2's turn :");
+                p2.play(p2);
+            }
+        }
+        playerWins(p1, p2);
+    }
+
+}
+
 public class App {
     private static final Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
         Player playerObj = new Player();
+        Player playerObj2 = new Player();
+        PlayWith2Players play = new PlayWith2Players();
         logger.info("Welcome Snake & Ladder Program!");
         logger.info("Intitial Player position is : " + playerObj.getPlayerPosition());
-        playerObj.play(playerObj);
+        play.playWith2Players(playerObj, playerObj2);
     }
+
 }
