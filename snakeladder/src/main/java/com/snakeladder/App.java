8ab9c2289046f9ccb3d5fe6a48f4f5bc81e8705c
RahulGaur998 <gaur.rahul996@gmail.com>
2022-09-17 10:29:08 +0530

    ADD: added functionality to generate random value to check if NoPlay/ Ladder/ Snake and return current playerPosition

--- snakeladder/src/main/java/com/snakeladder/App.java
+++ snakeladder/src/main/java/com/snakeladder/App.java
@@ -18,6 +18,26 @@ class Player {
         return (int) Math.floor(Math.random() * (6 - 1 + 1) + 1); // playerRollsDie to get value between 1-6
     }
 
+    int checkOption(int dieValue) { // checks whether no-play/ladder/snake using random and switch
+        int option = (int) Math.floor(Math.random() * (2 - 0 + 1) + 0);
+        switch (option) {
+            case 0: // if option generates 0 do nothing
+                break;
+            case 1:
+                setPlayerPosition(playerPosition + dieValue); // if option generates 1 add dieValue to playerPosition
+                break;
+            case 2:
+                playerPosition = playerPosition - dieValue; // if option generates 2 minus dievalue from playerPosition
+                if (playerPosition < 0) {
+                    setPlayerPosition(0); // if player position goes below zero assign 0 to playerPosition
+                } else {
+                    setPlayerPosition(playerPosition); // else setPlayer position
+                }
+                break;
+        }
+        return playerPosition; // return player position
+    }
+
     Player() { // constructor to initialize playerPosition as zero
         playerPosition = 0;
     }
@@ -30,6 +50,9 @@ public class App {
         Player playerObj = new Player();
         logger.info("Welcome Snake & Ladder Program!");
         logger.info("Intitial Player position is : " + playerObj.getPlayerPosition());
-        logger.info("Rolling a die player get : " + playerObj.playerRollsDie());
+        int dieValue = playerObj.playerRollsDie();
+        logger.info("Rolling a die player get : " + dieValue);
+        logger.info(
+                "After Checking options NoPlay/ Ladder/ Snake player position is: " + playerObj.checkOption(dieValue));
     }
 }
