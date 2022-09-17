abbd706a153216e4bf87fd07d8eca9e8d53672e2
RahulGaur998 <gaur.rahul996@gmail.com>
2022-09-17 09:39:07 +0530

    EDIT: Added Player class and added getter and setter for player position

--- snakeladder/src/main/java/com/snakeladder/App.java
+++ snakeladder/src/main/java/com/snakeladder/App.java
@@ -3,11 +3,28 @@ package com.snakeladder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+class Player {
+    int playerPosition;
+
+    public int getPlayerPosition() { // getter to get player position value
+        return playerPosition;
+    }
+
+    public void setPlayerPosition(int playerPosition) {
+        this.playerPosition = playerPosition;
+    }
+
+    Player() { // constructor to initialize playerPosition as zero
+        playerPosition = 0;
+    }
+}
+
 public class App {
     private static final Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
-
+        Player playerObj = new Player();
         logger.info("Welcome Snake & Ladder Program!");
+        logger.info("Intitial Player position is : " + playerObj.getPlayerPosition());
     }
 }
